1ae3dff756ee908f3edfdba6910791b4a2872b98
Ibrahim Assem <easim972@gmail.com>
2026-08-06 14:05:26 +0300

    Show weaving supplier on weaving order

--- documents.js
+++ documents.js
@@ -332,7 +332,7 @@
     }
 
     function weavingInfoSection(order) {
-      return `<div class="document-meta weaving-document-meta"><div><span>التاريخ</span>${safeText(order?.orderDate)}</div><div><span>العميل</span>${safeText(customerName(order))}</div><div><span>الصنف</span>${safeText(order?.fabricType)}</div><div><span>المصبغة</span>${safeText(order?.dyehouse)}</div></div>`;
+      return `<div class="document-meta weaving-document-meta"><div><span>التاريخ</span>${safeText(order?.orderDate)}</div><div><span>العميل</span>${safeText(customerName(order))}</div><div><span>الصنف</span>${safeText(order?.fabricType)}</div><div><span>مصنع / مورد النسيج</span>${safeText(order?.weavingSource)}</div></div>`;
     }
 
     function weavingRawComponents(order) {
